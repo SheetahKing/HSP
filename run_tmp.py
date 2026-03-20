b7f37638bc3f3dbabebea93414b85180bbe3eadd
SheetahKing <111549123+SheetahKing@users.noreply.github.com>
2026-03-20 14:05:34 +0800

    Add files via upload

---
+++ run_tmp.py
@@ -0,0 +1,28 @@
+
+from __future__ import absolute_import, unicode_literals, division, print_function
+
+import sys
+import os
+import time
+import argparse
+import random
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+from transformers import AutoTokenizer, AutoModel
+
+
+
+
+
+
+"""
+The complete trainer 
+code will be published 
+after the article is published!
+"""
+
+class HSPTrainer:
+    pass
